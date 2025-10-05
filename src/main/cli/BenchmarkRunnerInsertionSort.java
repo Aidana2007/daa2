9bb38ebcccea32d657b17cb930893abae3c9c823
20kami20 <kuchitarova15@gmail.com>
2025-10-05 19:43:14 +0300

    commit

--- src/main/cli/BenchmarkRunnerInsertionSort.java
+++ src/main/cli/BenchmarkRunnerInsertionSort.java
@@ -1,13 +1,13 @@
-package cli;
+package main.cli;
 
-import algorithms.InsertionSort;
-import metrics.PerformanceTrackerInsertionSort;
-
+import main.algorithms.InsertionSort;
+import main.metrics.PerformanceTrackerInsertionSort;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Random;
 
 public class BenchmarkRunnerInsertionSort {
+
     public static void main(String[] args) throws IOException {
         int[] sizes = {100, 1000, 10000};
         try (FileWriter writer = new FileWriter("benchmark_results.csv")) {
@@ -35,7 +35,9 @@ public class BenchmarkRunnerInsertionSort {
     private static int[] generateRandomArray(int n) {
         Random rand = new Random();
         int[] arr = new int[n];
-        for (int i = 0; i < n; i++) arr[i] = rand.nextInt(100000);
+        for (int i = 0; i < n; i++) {
+            arr[i] = rand.nextInt(100000);
+        }
         return arr;
     }
 }
